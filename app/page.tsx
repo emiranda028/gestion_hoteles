a4d86bb67457de26146e06cc4667b17638c6af94
Emmanuel Miranda <65192290+emiranda028@users.noreply.github.com>
2025-12-26 13:09:21 -0300

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px', fontFamily: 'sans-serif' }}>
+      <h1>¡Proyecto Hoteles v2!</h1>
+      <p>Si ves este mensaje, la estructura de carpetas es correcta.</p>
+    </div>
+  )
+}
